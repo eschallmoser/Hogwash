53d700919d69e91c858377e740e402fa3b1df335
Erin Schallmoser <dialogofadream@gmail.com>
2021-10-04 21:03:28 -0700

    add JavaScript variables and functions

---
+++ js-crashcourse/index.js
@@ -0,0 +1,20 @@
+// var, let, const
+// always use const, unless you know you're going to change the value
+
+// Primitive Data Types: String, Numbers, Boolean, null, undefined
+
+// const name = 'John';
+// const age = 30;
+
+// //Concatenation
+//  console.log('My name is ' + name + ' and I am ' + age + ' years old.');
+// //  String Interpolation with Template Literals
+//  console.log(`My name is ${name} and I am ${age} years old`)
+
+
+// const s = 'Hello World';
+// console.log(s.length);
+
+let doug = "Dinosaurs";
+
+console.log(doug[doug.length -1]);
