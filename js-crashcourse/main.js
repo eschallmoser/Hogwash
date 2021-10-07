17a8763c31e8cfe9e2adbd69d1ed4e7a18b70433
Erin Schallmoser <dialogofadream@gmail.com>
2021-10-07 07:55:39 -0700

    add events

--- js-crashcourse/main.js
+++ js-crashcourse/main.js
@@ -273,13 +273,69 @@
 
 // items.forEach((item) => console.log(item));
 
-const ul = document.querySelector('.items');
+// const ul = document.querySelector('.items');
 
 // ul.remove();
 
 // ul.firstElementChild.remove();
 // ul.lastElementChild.remove();
-ul.firstElementChild.textContent = 'Hello';
-// ul.lastElementChild.textContent = 'Goodbye';
-ul.children[1].textContent = 'The Middle';
-ul.lastElementChild.innerHTML = '<h1>Goodbye</h1>'
+// ul.firstElementChild.textContent = 'Hello';
+// // ul.lastElementChild.textContent = 'Goodbye';
+// ul.children[1].textContent = 'The Middle';
+// ul.lastElementChild.innerHTML = '<h1>Goodbye</h1>'
+
+// const btn = document.querySelector('.btn');
+// btn.style.background = 'red';
+
+// const btn = document.querySelector('.btn');
+
+// btn.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     document.querySelector('#my-form').style.background = '#ccc';
+//     document.querySelector('body').classList.add('bg-dark');
+//     document.querySelector('.items').lastElementChild.innerHTML = '<h1>Hello</h1>';
+// });
+
+// btn.addEventListener('mouseover', (e) => {
+//     e.preventDefault();
+//     document.querySelector('#my-form').style.background = '#ccc';
+//     document.querySelector('body').classList.add('bg-dark');
+//     document.querySelector('.items').lastElementChild.innerHTML = '<h1>Hello</h1>';
+// });
+
+// btn.addEventListener('mouseout', (e) => {
+//     e.preventDefault();
+//     document.querySelector('#my-form').style.background = '#ccc';
+//     document.querySelector('body').classList.add('bg-dark');
+//     document.querySelector('.items').lastElementChild.innerHTML = '<h1>Hello</h1>';
+// });
+
+const myForm = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const msg = document.querySelector('.msg');
+const userList = document.querySelector('#users');
+
+myForm.addEventListener('submit', onSubmit);
+
+function onSubmit (e) {
+    e.preventDefault();
+
+    if(nameInput.value === '' || emailInput.value === '') {
+        // alert('Please enter fields');
+        msg.classList.add('error');
+        msg.innerHTML = 'Please enter all fields';
+
+        setTimeout(() => msg.remove(), 3000)
+    } else {
+        // console.log('success');
+        const li = document.createElement('li');
+        li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`));
+
+        userList.appendChild(li);
+
+        // Clear fields
+        nameInput.value = '';
+        emailInput.value = '';
+    }
+}
